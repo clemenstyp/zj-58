63440f0a558c5acd8844d4f28801027a7133a032
belese <belese@belese.lan>
2017-10-12 14:51:37 +0200

    temporary clean

--- rastertozj.c
+++ rastertozj.c
@@ -17,23 +17,24 @@
 
 struct settings_
 {
-	int cashDrawer1;
-	int cashDrawer2;
-	int blankSpace;
-	int feedDist;
-	int heatingdots; 
-	int heatingtime;
-	int heatinginterval;
-	int printdensity;
-	int printbreaktime;
-	int sleepafter;
+    int cashDrawer1;
+    int cashDrawer2;
+    int blankSpace;
+    int feedDist;
+    int heatingdots;
+    int heatingtime;
+    int heatinginterval;
+    int printdensity;
+    int printbreaktime;
+    int sleepafter;
+    int softcontrol;
 };
 struct settings_ settings;
 
 struct command
 {
-	int    length;
-	char* command;
+    int    length;
+    char* command;
 };
 
 // define printer initialize command
@@ -46,7 +47,7 @@ static const struct command cashDrawerEject [2] =
  {5,(char[5]){0x1b,0x70,1,0x40,0x50}}};
 
 // define raster mode start command
-static const struct command rasterModeStartCommand =
+const struct command rasterModeStartCommand =
 {4,(char[4]){0x1d,0x76,0x30,0}};
 
 #ifdef DEBUGP
@@ -56,55 +57,55 @@ FILE* lfd = 0;
 // putchar with debug wrapper
 inline void mputchar(char c)
 {
-	unsigned char m = c;
+    unsigned char m = c;
 #ifdef DEBUGP
-	if (lfd)
-		fprintf (lfd,"%02x ",m);
+//  if (lfd)
+//      fprintf (lfd,"%02x ",m);
 #endif
-	putchar(m);
+    putchar(m);
 }
 
 // procedure to output an array
 inline void outputarray(const char * array, int length)
 {
-	int i = 0;
-	for (;i<length;++i)
-		mputchar(array[i]);
+    int i = 0;
+    for (;i<length;++i)
+        mputchar(array[i]);
 }
 
 // output a command
 inline void outputCommand(struct command output)
 {
-	outputarray(output.command,output.length);
+    outputarray(output.command,output.length);
 }
 
 inline int lo(int val)
 {
-	return val & 0xFF;
+    return val & 0xFF;
 }
 
 inline int hi (int val)
 {
-	return lo (val>>8);
+    return lo (val>>8);
 }
 
 // enter raster mode and set up x and y dimensions
 inline void rasterheader(int xsize, int ysize)
 {
-	outputCommand(rasterModeStartCommand);
-	mputchar(lo(xsize));
-	mputchar(hi(xsize));
-	mputchar(lo(ysize));
-	mputchar(hi(ysize));
+    outputCommand(rasterModeStartCommand);
+    mputchar(lo(xsize));
+    mputchar(hi(xsize));
+    mputchar(lo(ysize));
+    mputchar(hi(ysize));
 }
 
 // print all unprinted (i.e. flush the buffer)
 // then feed given number of lines
 inline void skiplines(int size)
 {
-	mputchar(0x1b);
-	mputchar(0x4a);
-	mputchar(size);
+    mputchar(0x1b);
+    mputchar(0x4a);
+    mputchar(size);
 }
 
 //Set “max heating dots”,”heating time”, “heating interval”
@@ -116,312 +117,360 @@ inline void skiplines(int size)
 //The more heating time, the more density , but the slower printing
 //speed. If heating time is too short, blank page may occur.
 //The more heating interval, the more clear, but the slower
-//printingspeed. 
+//printingspeed.
 inline void prtsleep()
 {
-	mputchar(0x1B);       // Esc
-	mputchar(0x38); 
-	mputchar(settings.sleepafter);       // 1 sec - 0 = don't sleep)
+#ifdef DEBUGP
+        if (lfd)
+            fprintf (lfd,"Sleep Printer");
+#endif
+
+    mputchar(0x1B);       // Esc
+    mputchar(0x38);
+    mputchar(settings.sleepafter);       // 1 sec - 0 = don't sleep)
 }
 
 inline void wakeup()
 {
-	if (settings.sleepafter != 0)
-	{
-	     mputchar(0xFF);
-	     usleep(50 * 1000);
-	}
+    if (settings.sleepafter != 0)
+    {
+#ifdef DEBUGP
+        if (lfd)
+            fprintf (lfd,"Wake Up Printer\n");
+#endif
+         mputchar(0xFF);
+         usleep(50 * 1000);
+    }
 }
 
-inline void setPrintSettings(char heatingdots,char heatingtime,char heatinginterval)
+inline void setPrintSettings(int heatingdots,int heatingtime,int heatinginterval)
 {
-	mputchar(0x1B);       // Esc
-	mputchar(0x37);       // 7 (print settings)
-	mputchar(heatingdots);      
-	mputchar(heatingtime);
-	mputchar(heatinginterval);	
+#ifdef DEBUGP
+    if (lfd)
+        fprintf (lfd,"Set printer Settings :\n heating dots = %d\nheating time = %d,heating interval = %d\n",heatingdots,heatingtime,heatinginterval);
+#endif
+    mputchar(0x1B);       // Esc
+    mputchar(0x37);       // 7 (print settings)
+    mputchar(heatingdots);
+    mputchar(heatingtime);
+    mputchar(heatinginterval);
 }
 
 //D4..D0 of n is used to set the printing density
 //Density is 50% + 5% * n(D4-D0) printing density
 //D7..D5 of n is used to set the printing break time
-//Break time is n(D7-D5)*250us 
-inline void setPrintDensity(char  printBreakTime,char printDensity)
+//Break time is n(D7-D5)*250us
+inline void setPrintDensity(int printDensity,int printBreakTime)
 {
-	mputchar(0x12);       // DC2
-	mputchar(0x23);       // Print density
-	mputchar(((char) printBreakTime << 5) | (char) printDensity);
+#ifdef DEBUGP
+    if (lfd)
+        fprintf (lfd,"Set printer Density :\n printDensity = %d\n printBreakTime = %d\n",printDensity,printBreakTime);
+#endif
+    mputchar(0x12);       // DC2
+    mputchar(0x23);       // Print density
+    mputchar(((char) printBreakTime << 5) | (char) printDensity);
 }
 
 // get an option
 inline int getOptionChoiceIndex(const char * choiceName, ppd_file_t * ppd)
 {
-	ppd_choice_t * choice;
-	ppd_option_t * option;
+    ppd_choice_t * choice;
+    ppd_option_t * option;
 
-	choice = ppdFindMarkedChoice(ppd, choiceName);
+    choice = ppdFindMarkedChoice(ppd, choiceName);
 
-	if (choice == NULL)
-	{
-		if ((option = ppdFindOption(ppd, choiceName))          == NULL) return -1;
-		if ((choice = ppdFindChoice(option,option->defchoice)) == NULL) return -1;
-	}
+    if (choice == NULL)
+    {
+        if ((option = ppdFindOption(ppd, choiceName))          == NULL) return -1;
+        if ((choice = ppdFindChoice(option,option->defchoice)) == NULL) return -1;
+    }
 
-	return atoi(choice->choice);
+    if (strcmp(choice->choice,"true"))
+        return 1;
+    if (strcmp(choice->choice,"false"))
+        return 0;
+
+    return atoi(choice->choice);
 }
 
 
 void initializeSettings(char * commandLineOptionSettings)
 {
-	ppd_file_t *    ppd         = NULL;
-	cups_option_t * options     = NULL;
-	int             numOptions  = 0;
+    ppd_file_t *    ppd         = NULL;
+    cups_option_t * options     = NULL;
+    int             numOptions  = 0;
 
-	ppd = ppdOpenFile(getenv("PPD"));
+    ppd = ppdOpenFile(getenv("PPD"));
 
-	ppdMarkDefaults(ppd);
+    ppdMarkDefaults(ppd);
 
-	numOptions = cupsParseOptions(commandLineOptionSettings, 0, &options);
-	if ((numOptions != 0) && (options != NULL))
-	{
-		cupsMarkOptions(ppd, numOptions, options);
-		cupsFreeOptions(numOptions, options);
-	}
+    numOptions = cupsParseOptions(commandLineOptionSettings, 0, &options);
+    if ((numOptions != 0) && (options != NULL))
+    {
+        cupsMarkOptions(ppd, numOptions, options);
+        for (int i=0;i<numOptions;i++)
+        {
+            fprintf (lfd,"%s - %s \n",options[i].name,options[i].value);
 
-	memset(&settings, 0x00, sizeof(struct settings_));
+        }
+        cupsFreeOptions(numOptions, options);
+    }
 
-	settings.cashDrawer1  = getOptionChoiceIndex("CashDrawer1Setting", ppd);
-	settings.cashDrawer2  = getOptionChoiceIndex("CashDrawer2Setting", ppd);
-	settings.blankSpace   = getOptionChoiceIndex("BlankSpace"        , ppd);
-	settings.feedDist     = getOptionChoiceIndex("FeedDist"          , ppd);
-	settings.heatingdots     = getOptionChoiceIndex("HeatingDots"          , ppd);
-	settings.heatingtime     = getOptionChoiceIndex("HeatingTime"          , ppd);
-	settings.heatinginterval     = getOptionChoiceIndex("HeatingInterval"          , ppd);
-	settings.printdensity   = getOptionChoiceIndex("Density"          , ppd);
-	settings.printbreaktime =  getOptionChoiceIndex("breaktime"          , ppd);
-	settings.sleepafter =  getOptionChoiceIndex("SleepAfter"          , ppd);
-	
+    memset(&settings, 0x00, sizeof(struct settings_));
 
-	ppdClose(ppd);
+    settings.cashDrawer1  = getOptionChoiceIndex("CashDrawer1Setting", ppd);
+    settings.cashDrawer2  = getOptionChoiceIndex("CashDrawer2Setting", ppd);
+    settings.blankSpace   = getOptionChoiceIndex("BlankSpace"        , ppd);
+    settings.feedDist     = getOptionChoiceIndex("FeedDist"          , ppd);
+    settings.sleepafter =  getOptionChoiceIndex("Sleep"          , ppd);
+    settings.softcontrol =  getOptionChoiceIndex("FlowControl"          , ppd);
+#ifdef DEBUGP
+    if (lfd)
+        fprintf (lfd,"get FlowControl : %d\n",settings.softcontrol);
+#endif
+
+    ppdClose(ppd);
 }
 
 // sent on the beginning of print job
 void jobSetup()
 {
-	wakeup();
-	outputCommand(printerInitializeCommand);
-	
-	if ( settings.cashDrawer1==1 )
-		outputCommand(cashDrawerEject[0]);
-	if ( settings.cashDrawer2==1 )
-		outputCommand(cashDrawerEject[1]);
-			
-	setPrintSettings(settings.heatingdots,settings.heatingtime,settings.heatinginterval);
-	setPrintDensity(settings.printdensity,settings.printbreaktime);
+    wakeup();
+    outputCommand(printerInitializeCommand);
 
+    if ( settings.cashDrawer1==1 )
+        outputCommand(cashDrawerEject[0]);
+    if ( settings.cashDrawer2==1 )
+        outputCommand(cashDrawerEject[1]);
 }
 
-inline void lineDelay(const char * array, int length)
+inline void lineDelay(const char * array, int length,int dotdelay)
 {
-	int i = 0;
-	float sleepus = 0;
-	int dotdelay = (settings.heatingtime * 10) + (settings.heatinginterval * 10) + (settings.printbreaktime * 250);
-	for (;i<length;++i)
-		sleepus += array[i]/255 * dotdelay;
-	usleep(sleepus);	
-}	
+    int i = 0;
+    float sleepus = 0;    
+    for (;i<length;++i)
+        sleepus += (unsigned int)array[i];
+#ifdef DEBUGP
+    if (lfd)
+        fprintf (lfd,"Software Flow Control, sleep for %f",sleepus);
+#endif
+    sleepus = (sleepus / 255) / length;
+#ifdef DEBUGP
+    if (lfd)
+        fprintf (lfd,"Software Flow Control, sleep for %f",sleepus);
+#endif
+    sleepus = sleepus * dotdelay;
+#ifdef DEBUGP
+    if (lfd)
+        fprintf (lfd,"Software Flow Control, sleep for %f",sleepus);
+#endif
+    usleep(sleepus/1000);
+}
 
 // sent at the very end of print job
 void ShutDown()
 {
-	if ( settings.cashDrawer1==2 )
-		outputCommand(cashDrawerEject[0]);
-	if ( settings.cashDrawer2==2 )
-		outputCommand(cashDrawerEject[1]);
-	outputCommand(printerInitializeCommand);
-	prtsleep();
+    if ( settings.cashDrawer1==2 )
+        outputCommand(cashDrawerEject[0]);
+    if ( settings.cashDrawer2==2 )
+        outputCommand(cashDrawerEject[1]);
+    outputCommand(printerInitializeCommand);
+    prtsleep();
 }
 
 // sent at the end of every page
 __sighandler_t old_signal;
 void EndPage()
 {
-	int i;
-	for (i=0; i<settings.feedDist; ++i)
-		skiplines(0x18);
-	signal(15,old_signal);
+    int i;
+    for (i=0; i<settings.feedDist; ++i)
+        skiplines(0x18);
+    signal(15,old_signal);
 }
 
 // sent on job canceling
 void cancelJob(int foo)
 {
-	int i=0;
-	for(;i<0x258;++i)
-		mputchar(0);
-	EndPage();
-	ShutDown();
+    int i=0;
+    for(;i<0x258;++i)
+        mputchar(0);
+    EndPage();
+    ShutDown();
 }
 
 // invoked before starting to print a page
 void pageSetup()
 {
-	old_signal = signal(15,cancelJob);
+    old_signal = signal(15,cancelJob);
 }
 
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-	int fd = 0;					// File descriptor providing CUPS raster data
-	cups_raster_t * ras = NULL; // Raster stream for printing
-	cups_page_header2_t header; // CUPS Page header
-	int page = 0;				// Current page
-	int y = 0;					// Vertical position in page 0 <= y <= header.cupsHeight
+    int fd = 0;                 // File descriptor providing CUPS raster data
+    cups_raster_t * ras = NULL; // Raster stream for printing
+    cups_page_header2_t header; // CUPS Page header
+    int page = 0;               // Current page
+    int y = 0;                  // Vertical position in page 0 <= y <= header.cupsHeight
 
-	unsigned char * rasterData   = NULL;	// Pointer to raster data from CUPS
-	unsigned char * originalRasterDataPtr   = NULL; // Copy of original pointer for freeing buffer
+    unsigned char * rasterData   = NULL;    // Pointer to raster data from CUPS
+    unsigned char * originalRasterDataPtr   = NULL; // Copy of original pointer for freeing buffer
 
-	if (argc < 6 || argc > 7)
-	{
-		fputs("ERROR: rastertozj job-id user title copies options [file]\n", stderr);
-		return EXIT_FAILURE;
-	}
+    if (argc < 6 || argc > 7)
+    {
+        fputs("ERROR: rastertozj job-id user title copies options [file]\n", stderr);
+        return EXIT_FAILURE;
+    }
 
-	if (argc == 7)
-	{
-		if ((fd = open(argv[6], O_RDONLY)) == -1)
-		{
-			perror("ERROR: Unable to open raster file - ");
-			sleep(1);
-			return EXIT_FAILURE;
-		}
-	} else
-		fd = 0;
-
-#ifdef DEBUGP
-	lfd = fopen ("/tmp/raster.txt","w");
-#endif
-
-	initializeSettings(argv[5]);
-	jobSetup();
-	ras = cupsRasterOpen(fd, CUPS_RASTER_READ);
-	page = 0;
-
-	while (cupsRasterReadHeader2(ras, &header))
-	{
-		if ((header.cupsHeight == 0) || (header.cupsBytesPerLine == 0))
-			break;
+    if (argc == 7)
+    {
+        if ((fd = open(argv[6], O_RDONLY)) == -1)
+        {
+            perror("ERROR: Unable to open raster file - ");
+            sleep(1);
+            return EXIT_FAILURE;
+        }
+    } else
+        fd = 0;
 
 #ifdef DEBUGP
-	if (lfd) fprintf(lfd,"\nheader.cupsHeight=%d, header.cupsBytesPerLine=%d\n",header.cupsHeight,header.cupsBytesPerLine);
+    lfd = fopen ("/tmp/raster.txt","w");
 #endif
 
-		if (rasterData == NULL)
-		{
-			rasterData = malloc(header.cupsBytesPerLine*24);
-			if (rasterData == NULL)
-			{
-				if (originalRasterDataPtr   != NULL) free(originalRasterDataPtr);
-					cupsRasterClose(ras);
-				if (fd != 0)
-			        close(fd);
-				return EXIT_FAILURE;
+    initializeSettings(argv[5]);
+    jobSetup();
+    ras = cupsRasterOpen(fd, CUPS_RASTER_READ);
+    page = 0;
 
-			}
-			originalRasterDataPtr = rasterData;  // used to later free the memory
-		}
+    while (cupsRasterReadHeader2(ras, &header))
+    {
+        if ((header.cupsHeight == 0) || (header.cupsBytesPerLine == 0))
+            break;
 
-		fprintf(stderr, "PAGE: %d %d\n", ++page, header.NumCopies);
-		pageSetup();
+        //Settings Head parameter
+        setPrintSettings(header.cupsInteger[0],header.cupsInteger[1],header.cupsInteger[2]);
+        setPrintDensity(header.cupsInteger[3],header.cupsInteger[4]);
+        int dotdelay = (header.cupsInteger[1]* 10) + (header.cupsInteger[2] * 10) + (header.cupsInteger[4] * 250);
 
-		int foo = ( header.cupsWidth > 0x180 ) ? 0x180 : header.cupsWidth;
-		foo = (foo+7) & 0xFFFFFFF8;
-		int width_size = foo >> 3;
 
 #ifdef DEBUGP
-	if (lfd) fprintf(lfd,"\nheader.cupsWidth=%d, foo=%d, width_size=%d\n",header.cupsWidth,foo,width_size);
+    if (lfd)
+    {
+        fprintf(lfd,"\nheader.cupsHeight=%d, header.cupsBytesPerLine=%d\n",header.cupsHeight,header.cupsBytesPerLine);
+        fprintf(lfd,"\nDots = %u\nheating time = %u\nheating interval = %u\ndensity = %u\break time =%u\n",header.cupsInteger[0],header.cupsInteger[1],header.cupsInteger[2],header.cupsInteger[3],header.cupsInteger[4]);
+    }
 #endif
-		y = 0;
-		int zeroy = 0;
 
-		while ( y < header.cupsHeight )
-		{
-			if ((y & 127) != 0)
-				fprintf(stderr, "INFO: Printing page %d, %d%% complete...\n", page, (100 * y / header.cupsHeight));
+        if (rasterData == NULL)
+        {
+            rasterData = malloc(header.cupsBytesPerLine*24);
+            if (rasterData == NULL)
+            {
+                if (originalRasterDataPtr   != NULL) free(originalRasterDataPtr);
+                    cupsRasterClose(ras);
+                if (fd != 0)
+                    close(fd);
+                return EXIT_FAILURE;
 
-			int rest = header.cupsHeight-y;
-			if (rest>24)
-				rest=24;
+            }
+            originalRasterDataPtr = rasterData;  // used to later free the memory
+        }
+
+        fprintf(stderr, "PAGE: %d %d\n", ++page, header.NumCopies);
+        pageSetup();
+
+        int foo = ( header.cupsWidth > 0x180 ) ? 0x180 : header.cupsWidth;
+        foo = (foo+7) & 0xFFFFFFF8;
+        int width_size = foo >> 3;
 
 #ifdef DEBUGP
-	if (lfd) fprintf(lfd,"\nProcessing block of %d, starting from %d lines\n",rest,y);
+    if (lfd) fprintf(lfd,"\nheader.cupsWidth=%d, foo=%d, width_size=%d\n",header.cupsWidth,foo,width_size);
 #endif
-			y+=rest;
+        y = 0;
+        int zeroy = 0;
 
-			if (y)
-			{
-				unsigned char * buf = rasterData;
-				int j;
-				for ( j=0; j<rest; ++j)
-				{
-					if (!cupsRasterReadPixels(ras,buf,header.cupsBytesPerLine))
-						break;
-					buf+=width_size;
-				}
-#ifdef DEBUGP
-	if (lfd) fprintf(lfd,"\nReaded %d lines\n",j);
-#endif
-				if (j<rest)
-					continue;
-			}
-			int rest_bytes = rest * width_size;
-			int j;
-			for(j=0; (j<rest_bytes) && !rasterData[j]; j++);
+        while ( y < header.cupsHeight )
+        {
+            if ((y & 127) != 0)
+                fprintf(stderr, "INFO: Printing page %d, %d%% complete...\n", page, (100 * y / header.cupsHeight));
+
+            int rest = header.cupsHeight-y;
+            if (rest>24)
+                rest=24;
 
 #ifdef DEBUGP
-	if (lfd) fprintf(lfd,"\nChecked %d bytes of %d for zero\n",j,rest_bytes);
+    if (lfd) fprintf(lfd,"\nProcessing block of %d, starting from %d lines\n",rest,y);
 #endif
+            y+=rest;
 
-			if (j>=rest_bytes)
-			{
-				++zeroy;
-				continue;
-			}
-
-			for (;zeroy>0;--zeroy)
-				skiplines(24);
-			
-			rasterheader(width_size,rest);
-			outputarray((char*)rasterData,rest_bytes);
-			skiplines(0);
-			lineDelay((char*)rasterData,rest_bytes);
-		}
-		
-		if (!settings.blankSpace)
-			for(;zeroy>0;--zeroy)
-				skiplines(24);
-
-		EndPage();
-	}
-
-	ShutDown();
-
-	if (originalRasterDataPtr   != NULL) free(originalRasterDataPtr);
-	cupsRasterClose(ras);
-	if (fd != 0)
-		close(fd);
-
-	if (page == 0)
-		fputs("ERROR: No pages found!\n", stderr);
-	else
-		fputs("INFO: Ready to print.\n", stderr);
+            if (y)
+            {
+                unsigned char * buf = rasterData;
+                int j;
+                for ( j=0; j<rest; ++j)
+                {
+                    if (!cupsRasterReadPixels(ras,buf,header.cupsBytesPerLine))
+                        break;
+                    buf+=width_size;
+                }
+#ifdef DEBUGP
+    if (lfd) fprintf(lfd,"\nReaded %d lines\n",j);
+#endif
+                if (j<rest)
+                    continue;
+            }
+            int rest_bytes = rest * width_size;
+            int j;
+            for(j=0; (j<rest_bytes) && !rasterData[j]; j++);
 
 #ifdef DEBUGP
-	if (lfd)
-		fclose(lfd);
+    if (lfd) fprintf(lfd,"\nChecked %d bytes of %d for zero\n",j,rest_bytes);
 #endif
 
-	return (page == 0)?EXIT_FAILURE:EXIT_SUCCESS;
+            if (j>=rest_bytes)
+            {
+                ++zeroy;
+                continue;
+            }
+
+            for (;zeroy>0;--zeroy)
+                skiplines(24);
+
+            rasterheader(width_size,rest);
+            outputarray((char*)rasterData,rest_bytes);
+            skiplines(0);
+            if (settings.softcontrol == 1)
+            {
+                lineDelay((char*)rasterData,rest_bytes,dotdelay);
+            }
+        }
+
+        if (!settings.blankSpace)
+            for(;zeroy>0;--zeroy)
+                skiplines(24);
+
+        EndPage();
+    }
+
+    ShutDown();
+
+    if (originalRasterDataPtr   != NULL) free(originalRasterDataPtr);
+    cupsRasterClose(ras);
+    if (fd != 0)
+        close(fd);
+
+    if (page == 0)
+        fputs("ERROR: No pages found!\n", stderr);
+    else
+        fputs("INFO: Ready to print.\n", stderr);
+
+#ifdef DEBUGP
+    if (lfd)
+        fclose(lfd);
+#endif
+
+    return (page == 0)?EXIT_FAILURE:EXIT_SUCCESS;
 }
 
 // end of rastertozj.c
